f3012f99042276cb9a6dbb899d960ca9f3de8928
Primahale <mahalepriyanka82@gmail.com>
2022-04-27 23:06:11 +0530

    stopwach

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Stopwatch from './componants/Stopwatch';
+   
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading">Google Timer</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+  
+export default App;
